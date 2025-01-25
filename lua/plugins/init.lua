6c867f6cf4aecd4ff532be5fca11492499451646
ymtec90 <ymfstec@gmail.com>
2025-01-25 10:48:42 -0300

    improved the support for ChatGPT.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -43,64 +43,6 @@ return {
     opts = {},
   },
   {
-    "nvim-neorg/neorg",
-    version = "^7",
-    -- build = ":Neorg sync-parsers",
-    event = "VeryLazy",
-    dependecies = {
-      {
-        "nvim-lua/plenary.nvim",
-      },
-      {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-          auto_install = true,
-          highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-          },
-        },
-        config = function(_, opts)
-          require("nvim-treesitter.configs").setup(opts)
-        end,
-      },
-    },
-    opts = {
-      load = {
-        ["core.defaults"] = {}, -- Loads default behavior
-        ["core.concealer"] = {
-          config = {
-            { icon_preset = "diamond" },
-          },
-        }, -- Adds pretty icons to your documents
-        ["core.keybinds"] = {
-          -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds
-          config = {
-            default_keybindings = true,
-            neorg_leader = "<Leader><Leader>",
-          },
-        }, -- Adds defaults keybindings
-        ["core.completion"] = {
-          config = {
-            engine = "nvim-cmp",
-            name = "[Norg]",
-          },
-        }, -- Enables support for completion plugins
-        ["core.ui"] = {},
-        ["core.ui.calendar"] = {},
-        ["core.journal"] = {}, -- Enables journal module
-        ["core.dirman"] = {
-          config = {
-            workspaces = {
-              notes = "~/documents/repos/notes",
-            },
-            default_workspace = "notes",
-          },
-        },
-      },
-    },
-  },
-  {
     "hrsh7th/cmp-cmdline",
     keys = { ":", "/", "?" },
     dependencies = { "hrsh7th/nvim-cmp" },
@@ -348,7 +290,7 @@ return {
     },
     config = function()
       require("chatgpt").setup {
-        api_key_cmd = "echo $openai_key",
+        api_key_cmd = "pass show openai",
       }
     end,
   },
