6cc6619a13d898abd3b68cd21b4770b0defd9b9b
ymtec90 <ymfstec@gmail.com>
2026-08-10 11:22:14 -0300

    added json formatter support

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,6 +3,7 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
+    json = { "prettier" },
     python = { "ruff" },
     javascript = { "prettier" },
     typescript = { "prettier" },
